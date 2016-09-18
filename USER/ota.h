35c117d39bd9021db44c4d2c86d0515f9391011c
dzbei <766752468@qq.com>
2016-09-18 15:24:19 +0800

    add ota and pure files

---
+++ USER/ota.h
@@ -0,0 +1,6 @@
+#ifndef __OTA_H_
+#define __OTA_H_
+#include "sys.h"
+#define OTA_HEAD      0xEE
+#define OTA_END				0x55
+#endif
---
+++ USER/pure.h
@@ -0,0 +1,5 @@
+#ifndef __PURE_H_
+#define __PURE_H_
+#include "sys.h"
+
+#endif
